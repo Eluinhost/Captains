319dfb34194345b3579235b61b90cd5730a9080a
HowdenG <graham_howden1@yahoo.co.uk>
2014-07-14 12:38:37 +0100

    basic logic

--- src/main/java/com/publicuhc/captains/Commands/StartDraftCommand.java
+++ src/main/java/com/publicuhc/captains/Commands/StartDraftCommand.java
@@ -1,8 +1,16 @@
 package com.publicuhc.captains.Commands;
 
 import com.publicuhc.captains.DraftMode;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 public class StartDraftCommand extends DraftModeCommand
 {
@@ -12,9 +20,89 @@ public class StartDraftCommand extends DraftModeCommand
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String s, String[] strings)
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
     {
-        //TODO
-        return false;
+        if(!sender.hasPermission("captains.draft.startdraft")) {
+            sender.sendMessage(ChatColor.RED + "You do not have permission to use this command");
+            return true;
+        }
+
+        if(draftMode.isInDraftMode()) {
+            sender.sendMessage(ChatColor.RED + "Draft mode is already in progress.");
+            return true;
+        }
+
+        int teamSize = -1;
+        boolean addAll = false;
+        List<Player> captainList = new ArrayList<Player>();
+
+        for(String arg : args) {
+
+            //check for `-f`
+            if(arg.equalsIgnoreCase("-f")) {
+                addAll = true;
+                continue;
+            }
+
+            //check for `-t=team_size`
+            if(arg.startsWith("-t=")) {
+                //remove the start stuffs
+                arg = arg.substring(3);
+
+                //read in the team size
+                try {
+                    teamSize = Integer.parseInt(arg);
+
+                    //check for positive > 0 value
+                    if(teamSize < 1)
+                        throw new NumberFormatException();
+                } catch (NumberFormatException ex) {
+                    sender.sendMessage(ChatColor.RED + "Must provide a valid number for the team size");
+                    return true;
+                }
+            }
+
+            //must be a player (captain) name
+            @SuppressWarnings("deprecation") Player player = Bukkit.getPlayer(arg);
+
+            if(null == player) {
+                sender.sendMessage(ChatColor.RED + "Player "+arg+" is not online!");
+                return true;
+            }
+        }
+
+        if(captainList.size() < 2) {
+            sender.sendMessage(ChatColor.RED + "Must provide at least 2 captains");
+            return true;
+        }
+
+        List<Player> availablePlayers = availableForPick();
+
+        //TODO remove captains from available players
+
+        //TODO check if team size is -1 and auto set it
+
+        //TODO start draft mode with the provided captains and player list
+
+        draftMode.startDraftMode();
+        sender.sendMessage(ChatColor.GOLD + "Draft mode started");
+        return true;
+    }
+
+    /**
+     * @return List of all online players that are available for pick (without permission captains.draft.spectate)
+     */
+    protected List<Player> availableForPick()
+    {
+        List<Player> playerList = Arrays.asList(Bukkit.getOnlinePlayers());
+
+        Iterator<Player> playerIterator = playerList.iterator();
+        while(playerIterator.hasNext()) {
+            if(playerIterator.next().hasPermission("captains.draft.spectate")) {
+                playerIterator.remove();
+            }
+        }
+
+        return playerList;
     }
 }
